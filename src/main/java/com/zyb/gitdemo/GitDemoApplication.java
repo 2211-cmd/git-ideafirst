f20ff3ff0c5a9417f42b9d79015812d4322e3e1e
zyb <zyb@outlook.com>
2022-06-15 20:45:20 +0800

    idea-git second

--- src/main/java/com/zyb/gitdemo/GitDemoApplication.java
+++ src/main/java/com/zyb/gitdemo/GitDemoApplication.java
@@ -8,6 +8,7 @@ public class GitDemoApplication {
 
     public static void main(String[] args) {
         System.out.println("\"Hello,git\" = " + "Hello,git");
+        System.out.println("\"Hello,git\" = " + "Hello,git2");
         SpringApplication.run(GitDemoApplication.class, args);
     }
 
