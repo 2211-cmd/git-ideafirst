717688ac1e20b10ef3ce10eddc5bb09340271737
zyb <zyb@outlook.com>
2022-06-16 09:22:18 +0800

    idea-git push ssh 2222 master

--- src/main/java/com/zyb/gitdemo/GitDemoApplication.java
+++ src/main/java/com/zyb/gitdemo/GitDemoApplication.java
@@ -12,7 +12,7 @@ public class GitDemoApplication {
         System.out.println("\"Hello,git\" = " + "Hello,git3");
         System.out.println("\"Hello,git\" = " + "Hello,git4,master");
         System.out.println("\"Hello,git\" = " + "Hello,github");
-        System.out.println("\"Hello,git\" = " + "Hello,github ssh idea");
+        System.out.println("\"Hello,git\" = " + "Hello,github ssh 222222222222222222");
         SpringApplication.run(GitDemoApplication.class, args);
     }
 
